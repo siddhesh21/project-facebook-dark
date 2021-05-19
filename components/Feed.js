707d98e2e78f357cecf479a39fec334c4ff5e1e3
Siddhesh Kankekar <kankekas@tcd.ie>
2021-05-19 08:56:40 +0100

    Added Stories + StoryCard components

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,24 @@
+import InputBox from "./InputBox";
+import Posts from "./Posts";
+import Stories from "./Stories";
+
 function Feed() {
-  return <div></div>;
+  return (
+    <div
+      className="flex-grow h-screen pb-44 pt-6 mr-4 
+    cl:mr-40 overflow-y-auto scrollbar-hide"
+    >
+      <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
+        {/** STORIES */}
+        <Stories />
+        {/** INPUT BOX */}
+        <InputBox />
+        {/** POSTS */}
+        <Posts />
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
+// 1:48:33
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -5,11 +5,11 @@ function HeaderIcon({ Icon }) {
     <div
       className="flex items-center cursor-pointer md:px-10 
     sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 
-    active:border-blue-500 group"
+    active:border-purple-600 group"
     >
       <Icon
         className="h-5 text-gray-500 text-center sm:h-7 mx-auto 
-      group-hover:text-blue-500"
+      group-hover:text-purple-600"
       />
     </div>
   );
