bcf0aaff2243cc1f928e1a7be2ca9f978cdbe7a8
Siddhesh Kankekar <kankekas@tcd.ie>
2021-05-20 17:44:18 +0100

    Built the Posts and Post component

--- components/Posts.js
+++ components/Posts.js
@@ -1,9 +1,26 @@
+import Post from "./Post";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+
 function Posts() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [realtimePosts] = useCollection(
+    db.collection("posts").orderBy("timestamp", "desc")
+  );
+  return (
+    <div>
+      {realtimePosts?.docs.map((post) => (
+        <Post
+          key={post.id}
+          name={post.data().name}
+          message={post.data().message}
+          email={post.data().email}
+          timestamp={post.data().timestamp}
+          image={post.data().image}
+          postImage={post.data().postImage}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
